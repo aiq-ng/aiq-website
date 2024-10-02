e2b448f85e5b146ac74853d6a7b609c3982f5feb
Gerald Ezenagu <egerald344@gmail.com>
2024-10-02 17:51:29 +0100

    fixed contents

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
   imports: [],
   templateUrl: './contact-us.component.html',
-  styleUrl: './contact-us.component.css'
+  styleUrl: './contact-us.component.css',
 })
 export class ContactUsComponent {
-
+  constructor(private router: Router) {};
+  route() {
+    this.router.navigate(['/']);
+  }
 }
