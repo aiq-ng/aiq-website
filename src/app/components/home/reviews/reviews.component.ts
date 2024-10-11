7721be24cca0a3a943d9356ebdc32f27cd9d678d
samshal <samueladeshina73@gmail.com>
2024-10-11 06:43:01 +0100

    ..

--- src/app/components/home/reviews/reviews.component.ts
+++ src/app/components/home/reviews/reviews.component.ts
@@ -7,74 +7,74 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [CommonModule, NgFor],
   templateUrl: './reviews.component.html',
-  styleUrl: './reviews.component.css'
+  styleUrl: './reviews.component.css',
 })
-export class ReviewsComponent  {
-
+export class ReviewsComponent {
   @ViewChild('reviewsCarousel') reviewsCarousel!: ElementRef;
 
-  reviews : any [] = [
-    {
-      img : '/assets/military.jpg',
-      text: 'The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here.',
-      name: 'Margaret Olaitan',
-      office: 'THE NIGERIAN ARMY GENERAL',
-      profile: '/assets/profile.jpg'
-    },
-    {
-      img : '/assets/military.jpg',
-      text: 'The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here.',
-      name: 'Margaret Olaitan',
-      office: 'THE NIGERIAN NAVY GENERAL',
-      profile: '/assets/profile.jpg'
-    },
-    {
-      img : '/assets/image 6.jpg',
-      text: 'The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here.',
-      name: 'Margaret Olaitan',
-      office: 'THE NIGERIAN NAVY GENERAL',
-      profile: '/assets/profile.jpg'
-    },
-    {
-      img : '/assets/image 6.jpg',
-      text: 'The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here.',
-      name: 'Margaret Olaitan',
-      office: 'THE NIGERIAN NAVY GENERAL',
-      profile: '/assets/profile.jpg'
-    },
-    {
-      img : '/assets/image 6.jpg',
-      text: 'The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here.',
-      name: 'Margaret Olaitan',
-      office: 'THE NIGERIAN NAVY GENERAL',
-      profile: '/assets/profile.jpg'
-    }
-  ]
+  reviews: any[] = [
+    // {
+    //   img : '/assets/military.jpg',
+    //   text: 'The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here.',
+    //   name: 'Margaret Olaitan',
+    //   office: 'THE NIGERIAN ARMY GENERAL',
+    //   profile: '/assets/profile.jpg'
+    // },
+    // {
+    //   img : '/assets/military.jpg',
+    //   text: 'The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here.',
+    //   name: 'Margaret Olaitan',
+    //   office: 'THE NIGERIAN NAVY GENERAL',
+    //   profile: '/assets/profile.jpg'
+    // },
+    // {
+    //   img : '/assets/image 6.jpg',
+    //   text: 'The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here.',
+    //   name: 'Margaret Olaitan',
+    //   office: 'THE NIGERIAN NAVY GENERAL',
+    //   profile: '/assets/profile.jpg'
+    // },
+    // {
+    //   img : '/assets/image 6.jpg',
+    //   text: 'The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here.',
+    //   name: 'Margaret Olaitan',
+    //   office: 'THE NIGERIAN NAVY GENERAL',
+    //   profile: '/assets/profile.jpg'
+    // },
+    // {
+    //   img : '/assets/image 6.jpg',
+    //   text: 'The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here.',
+    //   name: 'Margaret Olaitan',
+    //   office: 'THE NIGERIAN NAVY GENERAL',
+    //   profile: '/assets/profile.jpg'
+    // }
+  ];
 
-  reviewItem = this.reviews.map((review: any)=>({
+  reviewItem = this.reviews.map((review: any) => ({
     image: review.img,
     content: review.text,
     name: review.name,
     office: review.office,
-    profile: review.profile
-  }))
+    profile: review.profile,
+  }));
 
   constructor(private router: Router) {}
 
-  
   goLeft() {
-    this.reviewsCarousel.nativeElement.scrollBy({ left: -300, behavior: 'smooth' });
+    this.reviewsCarousel.nativeElement.scrollBy({
+      left: -300,
+      behavior: 'smooth',
+    });
   }
 
   goRight() {
-    this.reviewsCarousel.nativeElement.scrollBy({ left: 300, behavior: 'smooth' });
+    this.reviewsCarousel.nativeElement.scrollBy({
+      left: 300,
+      behavior: 'smooth',
+    });
   }
 
   navigateHandler() {
     this.router.navigate(['/company']);
   }
 }
-
-
-
-
--- src/app/components/home/solutions/solutions.component.ts
+++ src/app/components/home/solutions/solutions.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 interface Slide {
   h1: string;
@@ -13,42 +14,45 @@ interface Slide {
   standalone: true,
   imports: [NgFor, CommonModule],
   templateUrl: './solutions.component.html',
-  styleUrl: './solutions.component.css'
+  styleUrl: './solutions.component.css',
 })
 export class SolutionsComponent {
+  constructor(private router: Router) {}
 
-
+  route(page: string) {
+    this.router.navigateByUrl(page);
+  }
   slides: Slide[] = [
     {
       h1: 'Defence and Intelligence',
       p: ' Enhance national security and gain a competitive edge with AI intelligence gathering and enterprise solutions.',
       img: 'assets/image 34.jpg',
-      svg: 'assets/Security.svg'
+      svg: 'assets/Security.svg',
     },
     {
       h1: 'Researchers',
       p: ' Leverage AI to analyze large datasets and uncover new insights.',
       img: 'assets/image 32.jpg',
-      svg: 'assets/Research.svg'
+      svg: 'assets/Research.svg',
     },
     {
-      h1: 'Humanitarian',
+      h1: 'Humanitarian Organizations',
       p: 'Improve disaster response and resource allocation with real-time data insights.',
       img: 'assets/image 31.jpg',
-      svg: 'assets/Give Money.svg'
+      svg: 'assets/Give Money.svg',
     },
     {
       h1: 'Media and Information',
       p: ' Investigate stories with advanced research tools and access critical information quickly to support investigative reporting.',
       img: 'assets/image 30.jpg',
-      svg: 'assets/megaphone.svg'
+      svg: 'assets/megaphone.svg',
     },
     {
-      h1: 'Other Businesses',
+      h1: 'Contact us to discuss your requirements.',
       p: ' Our suites of products are also used by Public and Private Businesses, Law Enforcement Agencies, Hospitals, and the General Public.',
       img: 'assets/image 52.jpg',
-      svg: 'assets/categories.svg'
-    }
+      svg: 'assets/categories.svg',
+    },
   ];
 
   currentSlideIndex = 0;
@@ -57,8 +61,11 @@ export class SolutionsComponent {
     this.currentSlideIndex = index;
   }
 
-   x = ["Defence and Intelligence", "Researchers", "Humanitarians", "Media", "Others"];
+  x = [
+    'Defence and Intelligence',
+    'Researchers',
+    'Humanitarians',
+    'Media',
+    'Others',
+  ];
 }
-
-
-
